52f99a9e59589576954b7b44db6d879af342e5e6
zhe0311 <1634549350@qq.com>
2022-05-27 02:52:56 +0800

    add: acknowledgements

--- code/highD-data-ana/cyz_ana/main.py
+++ code/highD-data-ana/cyz_ana/main.py
@@ -2,6 +2,7 @@ from cmath import nan
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import stats
 
 from read_data import read_track_csv
 
@@ -12,21 +13,54 @@ if __name__ == '__main__':
     data_path = "../../../../Desktop/highD-dataset-v1.0/data/01_tracks.csv"
     tracksInfo = get_data(data_path)
 
-    # plot
+    # plot setting
+    plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
 
-    index = 2
-    t = [i for i in range(len(tracksInfo[index]["xVelocity"]))]
-    
-    fig = plt.figure()
+    speedUp = []
+    speedDown = []
+    indexList = ["thw", "ttc", "dhw"]
+    index = indexList[0]
 
-    ax1 = fig.add_subplot(111)
-    ax1.plot(t, tracksInfo[index]["xAcceleration"])
-    
-    ax2 = ax1.twinx()
-    ax2.plot(t, tracksInfo[index]["thw"])
-    ax2.plot(t, tracksInfo[index]["ttc"])
+    # print(len(tracksInfo))
 
-    ax1.legend(["xVelocity"])
-    ax2.legend(["thw", "ttc"])
+    for track in tracksInfo:
+        for idx, index_ in enumerate(track[index]):
+            #  if track["followingId"][idx] != 0 and track["precedingId"][idx] != 0:  # 既在跟随，又被跟随
+            if track["precedingId"][idx] != 0: # 如果有前车
+                followingId = track["followingId"][idx]
+                frame = track["frame"][idx]
+                followingDis = 0
+                if followingId != 0: # 如果有跟随车辆
+                    beginFrame = tracksInfo[followingId-1]["frame"][0]
+                    followingDis = tracksInfo[followingId-1]["dhw"][frame-beginFrame]
+                if sum(track["followingId"]) == 0 or followingDis > 150:
+                    if track["xAcceleration"][idx] > 0:
+                        speedUp.append(index_)
+                    elif track["xAcceleration"][idx] < 0:
+                        speedDown.append(index_)
+
+    print(np.mean(speedUp), np.mean(speedDown))
+
+    speedUp_ = []
+    speedDown_ = []
+
+    # for i in range(len(speedUp)):
+    #     if speedUp[i] >= -2000 and speedUp[i] <= 2000:
+    #         speedUp_.append(speedUp[i])
+    # for i in range(len(speedDown)):
+    #     if speedDown[i] >= -2000 and speedDown[i] <= 2000:
+    #         speedDown_.append(speedDown[i])
+
+    print(stats.ttest_ind(speedUp, speedDown))
+    # print(speedUp)
+    plt.hist(speedUp, bins=30)
+
+    plt.xlabel(index)
+    plt.ylabel("频次")
     plt.show()
-    
+
+    plt.hist(speedDown, bins=30)
+
+    plt.xlabel(index)
+    plt.ylabel("频次")
+    plt.show()
